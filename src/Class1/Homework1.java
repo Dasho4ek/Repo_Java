f5fbb0886f59f9436d90239de379b3baa1f6d6c0
Dasho4ek <pdariya@gmail.com>
2021-07-05 18:49:51 -0400

    Homework1

---
+++ src/Class1/Homework1.java
@@ -0,0 +1,44 @@
+package Class1;
+
+
+public class Homework1 {
+    public static void main(String[] args) {
+        byte hoursInDay = 24;
+        System.out.println( "There are 24 hours in a day");
+
+        short maxNumDaysInYear = 366;
+        System.out.println( "There are 366 maximum days in a year");
+
+        int numEmpInOrg = 2000;
+        System.out.println( "Total number of employees in organization is " + 2000);
+
+        long populationInCountry = 54000000L;
+        System.out.println("Population in a country is " +54000000 );
+
+        float mortgageRate = 5.2f;
+        System.out.println(" Mortgage rate is " + 5.4 + "%");
+
+        double accountBalance = 17265.03;
+        System.out.println("Balance in a bank is " + 17265.03);
+
+        boolean doesSunRiseFromWest = false;
+        System.out.println("Does the sun rise from the west?: " + doesSunRiseFromWest );
+
+        char firstInitialOfMyName = 'D';
+        char secondInitialOfMyName = 'P';
+        System.out.println("Initials of my name is " + firstInitialOfMyName + secondInitialOfMyName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }}
